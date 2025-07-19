78d082de0e43d523cf5cba0c042e2717f7152574
alnoobel <noobel@bk.ru>
2025-07-19 23:41:16 +0500

    Fix port binding for Render

--- main.py
+++ main.py
@@ -55,4 +55,5 @@ def chat():
     return jsonify({'response': response})
 
 if __name__ == '__main__':
-    app.run(port=9696, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
